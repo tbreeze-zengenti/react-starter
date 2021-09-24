0d38577ff766981d447774284e7b2f29d80a32de
nflatley-zengenti <n.flatley@zengenti.com>
2021-09-24 02:10:57 +0100

    build: add polyfills to fix IE11 errors

--- src/client/polyfills.legacy.js
+++ src/client/polyfills.legacy.js
@@ -1,3 +1,22 @@
+import { enableES5 } from 'immer';
+
+enableES5();
+
+/* eslint-disable no-undef */
+// The polyfill starts here.
+(function () {
+  if (typeof globalThis === 'object') return;
+  Object.defineProperty(Object.prototype, '__magic__', {
+    get: function () {
+      return this;
+    },
+    configurable: true,
+  });
+  __magic__.globalThis = __magic__;
+  delete Object.prototype.__magic__;
+})();
+// The polyfill ends here.
+
 export default function loadPolyfills() {
   const fillFetch = () =>
     new Promise(resolve => {
