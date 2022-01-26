7db6ca88e8cb86ab1bd0b5bb7ebf2756c4192686
Tim Breeze <t.breeze@zengenti.com>
2022-01-26 23:49:02 +0000

    wip: Amended MediaQuery sizing variables. Removed grid variables. Needs more consideration.

--- src/app/theme/layout.ts
+++ src/app/theme/layout.ts
@@ -1,9 +1,9 @@
 export const sizes = {
   mobile: '320px',
-  tablet: '481px',
-  laptop: '769px',
-  desktop: '1025px',
-  largeDesktop: '1441px',
+  tablet: '480px',
+  laptop: '768px',
+  desktop: '1024px',
+  largeDesktop: '1440px',
 };
 
 export const mq = {
@@ -19,23 +19,23 @@ export const mq = {
 //   Styles here
 // }
 
-export const grid = {
-  gap: '1.2rem',
-  padding: {
-    default: '1.6rem',
-    large: '5.2rem',
-  },
-  margin: {
-    default: '1.6rem',
-    large: '5.2rem',
-  },
-  width: {
-    default: '170rem',
-    fullWidth: '100%',
-    article: '80rem',
-    small: '58.8rem',
-  },
-};
+// export const grid = {
+//   gap: '1.2rem',
+//   padding: {
+//     default: '1.6rem',
+//     large: '5.2rem',
+//   },
+//   margin: {
+//     default: '1.6rem',
+//     large: '5.2rem',
+//   },
+//   width: {
+//     default: '170rem',
+//     fullWidth: '100%',
+//     article: '80rem',
+//     small: '58.8rem',
+//   },
+// };
 
 export const spacing = {
   small: '4rem',
