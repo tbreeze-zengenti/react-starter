2554fba4ea5827d3aa5efc2ee27281fa8ba05c4d
Tim Breeze <t.breeze@zengenti.com>
2022-01-26 23:48:25 +0000

    fix: Updated GlobalStyles with Andy Bell's modern CSS reset [https://piccalil.li/blog/a-modern-css-reset/]

--- src/app/theme/globalStyles.ts
+++ src/app/theme/globalStyles.ts
@@ -3,60 +3,90 @@ import { normalize } from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
   /* Normalize/reset css */
+  /* Comment out if you do not wish to load it */
   ${normalize}
-	*,
-	*:before,
-	*:after {
-		box-sizing: border-box;
-	}
 
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  /* Remove default margin */
+  body,
+  h1,
+  h2,
+  h3,
+  h4,
+  p,
+  figure,
+  blockquote,
+  dl,
+  dd {
+    margin: 0;
+  }
+
+  /* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+  ul[role='list'],
+  ol[role='list'] {
+    list-style: none;
+  }
+
+  /* this sets 1rem to 10px */
   html, body {
     font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-    /* this sets 1rem to 10px */
     font-size: 62.5%;
   }
 
-  body{
+  /* Set core root defaults */
+  html:focus-within {
+    scroll-behavior: smooth;
+  }
+
+  /* Sets base font to 16px */
+  body {
+    min-height: 100vh;
     font-size: 1.6rem;
     font-weight: normal;
+    line-height: 1.5;
+    text-rendering: optimizeSpeed;
   }
 
-  h1, .h1{
-    font-size: 5.6rem;
-    line-height: 6.4rem;
-    font-weight: 500;
+  /* A elements that don't have a class get default styles */
+  a:not([class]) {
+    text-decoration-skip-ink: auto;
   }
 
-  h2, .h2{
-    font-size: 3.2rem;
-    line-height: 4rem;
-    font-weight: 500;
+  /* Make images easier to work with */
+  img,
+  picture {
+    max-width: 100%;
+    display: block;
   }
 
-  h3, .h3{
-    font-size: 2.4rem;
-    line-height: 3.2rem;
-    font-weight: 500;
+  /* Inherit fonts for inputs and buttons */
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
   }
 
-  h4, .h4{
-    font-size: 2rem;
-    line-height: 2.4rem;
-    font-weight: 500;
+  /* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+  @media (prefers-reduced-motion: reduce) {
+    html:focus-within {
+    scroll-behavior: auto;
+    }
+    
+    *,
+    *::before,
+    *::after {
+      animation-duration: 0.01ms !important;
+      animation-iteration-count: 1 !important;
+      transition-duration: 0.01ms !important;
+      scroll-behavior: auto !important;
+    }
   }
-
-  h5, .h5{
-    font-size: 2rem;
-    line-height: 2.4rem;
-    font-weight: normal;
-  }
-  
-  h6, .h6{
-    font-size: 1.6rem;
-    line-height: 2.4rem;
-    font-weight: normal;
-  }
-
 `;
 
 export default GlobalStyle;
