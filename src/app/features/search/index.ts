20f96df1937bbf4db984ecd24f04b8b7e188d8f4
Tim Breeze <t.breeze@zengenti.com>
2022-03-17 12:29:10 +0000

    chore: Removed SearchContainers/ListingContainers to reflect updates to the search package

--- src/app/features/search/index.ts
+++ src/app/features/search/index.ts
@@ -1,7 +1,1 @@
-export { default as config } from './config/search.config';
-
-// import ListingContainer from './listing.container';
-// export default ListingContainer;
-
-import SearchContainer from './containers/search.container';
-export default SearchContainer;
+export { default as config } from './search.config';
