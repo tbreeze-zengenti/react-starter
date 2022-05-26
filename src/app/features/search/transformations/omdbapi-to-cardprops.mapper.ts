8c53e5a10dce2fae352ce775481d30459c0da008
Derry Coffey <d.coffey@zengenti.com>
2022-05-26 16:50:06 +0100

    chore: Tighten types on omdbapi mapper

--- src/app/features/search/transformations/omdbapi-to-cardprops.mapper.ts
+++ src/app/features/search/transformations/omdbapi-to-cardprops.mapper.ts
@@ -1,5 +1,19 @@
-export default (body: any) =>
-  body?.Search?.map((r: any) => ({
+interface OMDBApiSearchResultItem {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+}
+
+interface OMDBApiSearchResult {
+  Search?: OMDBApiSearchResultItem[];
+  totalResults?: string;
+  Response?: string;
+}
+
+export default (body: OMDBApiSearchResult) =>
+  body?.Search?.map((r: OMDBApiSearchResultItem) => ({
     title: r.Title + ' ' + r.Year,
     description: '',
     image: r.Poster && r.Poster !== 'N/A' ? r.Poster : null,
