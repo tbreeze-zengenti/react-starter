69a3f457feb84e221b86b2ef172df1309bf255c6
t.breeze <t.breeze@zengenti.com>
2022-06-29 12:55:36 +0100

    chore: Added JSDoc @returns to provide feedback to spacing variables

--- src/app/theme/styled.d.ts
+++ src/app/theme/styled.d.ts
@@ -39,14 +39,23 @@ declare module 'styled-components' {
       };
     };
     spacing: {
+      /** @returns 0.8rem */
       xxxs: string;
+      /** @returns 1.6rem */
       xxs: string;
+      /** @returns 2.4rem */
       xs: string;
+      /** @returns 3.2rem */
       s: string;
+      /** @returns 4em */
       m: string;
+      /** @returns 6.4rem */
       l: string;
+      /** @returns 8rem */
       xl: string;
+      /** @returns 12rem */
       xxl: string;
+      /** @returns 14.4rem */
       xxxl: string;
     };
   }
