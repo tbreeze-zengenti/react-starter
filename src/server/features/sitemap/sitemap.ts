7322c6fc92b72af93ddeea2368eaae4587b23da8
d.petrovic <d.petrovic@zengenti.com>
2023-03-24 13:50:14 +0000

    fix: Search bug in sitemap feature

--- src/server/features/sitemap/sitemap.ts
+++ src/server/features/sitemap/sitemap.ts
@@ -1,4 +1,4 @@
-import { Op, Query } from 'contensis-delivery-api';
+import { Op, OrderBy, Query } from 'contensis-delivery-api';
 import { cachedSearch } from '~/core/util/ContensisDeliveryApi';
 import { dynamicSort } from '~/core/util/helpers';
 import { streamToPromise, SitemapStream } from 'sitemap';
@@ -53,6 +53,10 @@ const query = (pageIndex: number, pageSize: number) => {
 
   query.pageSize = pageSize;
   query.pageIndex = pageIndex;
+
+  /* We need an orderBy, otherwise result ordering is volatile and
+   changes from page to page, leading to duplicate (or missing) results */
+  query.orderBy = OrderBy.asc('sys.contentTypeId');
   return query;
 };
 
