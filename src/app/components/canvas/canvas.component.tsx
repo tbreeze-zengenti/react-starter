b78d1463f592820864e029d8ced2c14b77dc5160
ZenSamClifford <s.clifford@zengenti.com>
2023-12-20 09:24:01 +0000

    chore: Make canvas data required

--- src/app/components/canvas/canvas.component.tsx
+++ src/app/components/canvas/canvas.component.tsx
@@ -7,7 +7,7 @@ import {
 
 // Recommended reading: https://github.com/contensis/canvas/blob/main/apps/react/README.md
 
-const Canvas = ({ data }: { data?: Block[] }) => {
+const Canvas = ({ data }: { data: Block[] }) => {
   if (!data) return null;
 
   return (
