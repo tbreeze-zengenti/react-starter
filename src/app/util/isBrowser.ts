a26fe72f388be178c56c63d69a01ec2674e910c7
t.breeze <t.breeze@zengenti.com>
2024-03-21 09:32:53 +0000

    chore: Remove "extra" utilities

--- src/app/util/isBrowser.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Checks if the code is running in a browser environment.
- * @returns {boolean} Returns true if the code is running in a browser environment, otherwise returns false.
- */
-export const isBrowser = (): boolean => typeof window !== 'undefined';
