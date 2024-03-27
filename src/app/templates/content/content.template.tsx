c1c0cd308fa9c1b6bc706cefb0b1b7bfb5051a46
ZenSamClifford <s.clifford@zengenti.com>
2024-03-27 10:05:46 +0000

    chore: content template tweeks

--- src/app/templates/content/content.template.tsx
+++ src/app/templates/content/content.template.tsx
@@ -20,24 +20,22 @@ const Content = ({ mappedEntry }: MappedEntry<ContentTemplateProps>) => {
   const { meta, title, body } = mappedEntry || {};
   console.log(body);
   return (
-    <MainLayout meta={meta}>
-      <>
-        <Helmet>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-            rel="stylesheet"
-          ></link>
-        </Helmet>
-        <ContentPageStyled>
+    <>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Helmet>
+      <MainLayout meta={meta}>
+        <ContentTemplateStyled>
           <div className="wrapper">
             <div className="logo">
               <h1>{title}</h1>
             </div>
-            <div className="composer">
-              <Composer {...body} />
-            </div>
+            <Composer {...body} />
           </div>
         </ContentTemplateStyled>
       </MainLayout>
