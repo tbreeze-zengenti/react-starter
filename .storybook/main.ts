a7c8bd94ede55ec1e9d9aceb5bba30d5b85f23c7
ZenSamClifford <s.clifford@zengenti.com>
2024-03-27 10:54:47 +0000

    chore: storybook upgrade

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,13 @@ const config: StorybookConfig = {
   framework: '@storybook/react-webpack5',
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   // Optional
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-actions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-webpack5-compiler-babel'
+  ],
   docs: {
     autodocs: 'tag',
   },
