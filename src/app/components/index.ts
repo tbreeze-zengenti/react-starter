9ad4e7e3aafdbd5db4f3e790f7d72d02aea98041
ZenSamClifford <s.clifford@zengenti.com>
2024-03-28 11:23:07 +0000

    feat: type defs for components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,23 +1,20 @@
 import loadable from '@loadable/component';
-import { ComposerProps } from './composer/composer.types';
-import { CanvasProps } from '~/components/canvas/canvas.component';
-import { MarkupProps } from '~/components/markup/markup.component';
 
-export const Composer = loadable<ComposerProps>(
+export const Composer = loadable(
   () =>
     import(
       /* webpackChunkName: "composer" */ '~/components/composer/composer.component'
     )
 );
 
-export const Canvas = loadable<CanvasProps>(
+export const Canvas = loadable(
   () =>
     import(
       /* webpackChunkName: "canvas" */ '~/components/canvas/canvas.component'
     )
 );
 
-export const Markup = loadable<MarkupProps>(
+export const Markup = loadable(
   () =>
     import(
       /* webpackChunkName: "markup" */ '~/components/markup/markup.component'
