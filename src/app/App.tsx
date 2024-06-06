c9a4c853b012ba230dc9fb9aec1b98226796ffb4
ZenSamClifford <s.clifford@zengenti.com>
2024-06-06 11:23:25 +0100

    chore: Spacing on comment

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,18 +13,17 @@ import SkipToMainContent from '~/components/skipToMainContent/skipToMainContent.
 import PageLoader from '~/components/pageLoader/pageLoader.component';
 
 const AppRoot = (props: AppRootProps) => {
-  /*
-        *notFoundComponent={NotFound}*
-        This 404 Page / notFoundComponent is for local development purposes only.
-        To see this working on your live site, you will need to add this to the load balancer, to do this, please follow the steps below.
+  /** notFoundComponent={NotFound}*
+    This 404 Page / notFoundComponent is for local development purposes only.
+    To see this working on your live site, you will need to add this to the load balancer, to do this, please follow the steps below.
 
-        1. Go to Contensis.com.
-        2. Login and go to the help desk.
-        3. Raise a new support request.
-        4. Complete the form and give details of what error page you'd like to add e.g. 404, 503 etc.
-        5. Upload the error page(s) as raw html files.
-        6. Submit your request.
-      */
+    1. Go to Contensis.com.
+    2. Login and go to the help desk.
+    3. Raise a new support request.
+    4. Complete the form and give details of what error page you'd like to add e.g. 404, 503 etc.
+    5. Upload the error page(s) as raw html files.
+    6. Submit your request.
+  */
 
   return (
     <div id="app-root">
