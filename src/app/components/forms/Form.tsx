9fd30046f4457a7725858a146338d83ea7b32734
Neil Flatley <n.flatley@zengenti.com>
2024-07-08 18:01:07 +0100

    test: remove apiUrl prop as requests to forms api should be handled the same as the delivery api

--- src/app/components/forms/Form.tsx
+++ src/app/components/forms/Form.tsx
@@ -3,12 +3,7 @@ import { ContensisForm } from '@contensis/forms';
 
 const Form = ({ formId }: { formId: string }) => {
   const projectId = PROJECT; /* global PROJECT */
-  const apiUrl = SERVERS.cms; /* global SERVERS */
-  return (
-    <>
-      <ContensisForm apiUrl={apiUrl} projectId={projectId} formId={formId} />
-    </>
-  );
+  return <ContensisForm projectId={projectId} formId={formId} />;
 };
 
 export default Form;
