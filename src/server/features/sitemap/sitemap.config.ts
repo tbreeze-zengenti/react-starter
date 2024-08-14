449ea90025209a6af01e47a9b50fd37542cb2948
t.breeze <t.breeze@zengenti.com>
2024-08-14 22:46:04 +0100

    fix: Remove sitemap testing config params

--- src/server/features/sitemap/sitemap.config.ts
+++ src/server/features/sitemap/sitemap.config.ts
@@ -1,22 +1,11 @@
 import { SitemapConfig } from './sitemap';
-import { contentTypes } from '~/schema/contentTypes.schema';
 import { noIndexField } from '~/schema/fields.schema';
 
 export const sitemapConfig: SitemapConfig = {
   languages: ['en-GB'],
   noIndexField,
-  priorityMap: [
-    {
-      url: '/',
-      priority: 1,
-      changefreq: 'weekly',
-    },
-    {
-      url: contentTypes.contentPage,
-      priority: 0.5,
-    },
-  ],
+  priorityMap: [],
   additions: [],
-  excludeContentTypes: ['fieldLinkDepthTest'],
+  excludeContentTypes: [],
   excludePaths: ['/404', '/zenInfo'],
 };
