0f82a3aee84efb6889351a870942f130c5a1feba
t.breeze <t.breeze@zengenti.com>
2024-09-23 14:24:18 +0100

    chore: Bump CRB

--- src/app/routes/contentTypeRoutes.ts
+++ src/app/routes/contentTypeRoutes.ts
@@ -10,7 +10,6 @@ import { contentTypes } from '~/schema/contentTypes.schema';
  */
 const contentTypeRoutes: ContentTypeRoute[] = [
   {
-    /** If connected to Leif (default), visit `/about` */
     contentTypeID: contentTypes.contentPage,
     component: Content,
     entryMapper: mapRouteEntryToProps(contentMapper),
