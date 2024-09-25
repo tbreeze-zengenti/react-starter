1cba05f26d0813484ddeb7c36eb1cc4aab27eb4c
p.edbrooke <p.edbrooke@zengenti.com>
2024-09-25 15:43:11 +0100

    wip: video embed

--- src/app/components/videoEmbed/videoEmbed.component.tsx
+++ src/app/components/videoEmbed/videoEmbed.component.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export type VideoEmbedProps = {
+type VideoEmbedProps = {
   src: string;
   title: string;
+  caption?: string;
+  height?: number;
+  width?: number;
 };
 
-const VideoEmbed = ({ src, title }: VideoEmbedProps) => {
+const VideoEmbed = ({
+  src,
+  title,
+  caption,
+  height,
+  width,
+}: VideoEmbedProps) => {
   return (
     <VideoEmbedStyled className="video-embed">
       <IFrameStyled
         title={title}
         src={src}
         allowFullScreen
-        height={360}
-        width={640}
+        height={height ? height : 360}
+        width={width ? width : 640}
         loading="lazy"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         className="video-embed__iframe"
       ></IFrameStyled>
+      {caption && (
+        <CaptionStyled className="video-embed__caption">
+          {caption}
+        </CaptionStyled>
+      )}
     </VideoEmbedStyled>
   );
 };
@@ -28,3 +42,5 @@ export default VideoEmbed;
 const VideoEmbedStyled = styled.div``;
 
 const IFrameStyled = styled.iframe``;
+
+const CaptionStyled = styled.figcaption``;
